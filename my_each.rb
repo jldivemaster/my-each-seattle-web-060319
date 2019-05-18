257b6da8e0f1b5a0cbec8c0e71e8d4e5ee173889
Jason Leach <jlspursfan@gmail.com>
2019-05-18 16:15:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,10 @@ def my_each(array)
   i = 0
   new_array = []
   while i < array.length
-    new_array << array[i]
+    yield(array[i])
     i += 1
   end
   return new_array
 end
 
-#my_each(array) { new_array << array[i] }
+my_each(array[i]) { new_array << array[i] }
